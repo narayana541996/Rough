aca1a4ec1d8279f20c58f0322e86a8b3cb13af8c
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2020-12-29 03:51:02 -0800

    Add files via upload

--- scpp2.py
+++ scpp2.py
@@ -1,20 +1,23 @@
 from subprocess import call
-def scp_from_into(source_key_file_path = None, target_key_file_path, source_username, source_ip, source_filepath, file_path, target_username, target_ip, target_directory):#source_ip, target_ip, file_path, ssh_key
+def scp_from_into(source_username, source_ip, source_filepath, file_path, target_username, target_ip, target_directory, source_key_file_path = None, target_key_file_path = None):#source_ip, target_ip, file_path, ssh_key
     #print(call(r'scp -i C:\Users\krish\Downloads\docker.pem  C:\Users\krish\Downloads\Affiliation_BYE_LAWS.doc root@134.209.148.94:/root/'.split(' ')))
     #copy target_key_file to the source
     if target_key_file_path and not source_key_file_path:#If only target_key_file_path is given, same key is used for source_key_file_path
         source_key_file_path = target_key_file_path
     elif source_key_file_path and not target_key_file_path:#If only source_key_file_path is given, same key is used for target_key_file_path
         target_key_file_path = source_key_file_path
+    print(r'scp -i {} {} {}@{}:{}'.format(source_key_file_path,target_key_file_path, source_username, source_ip, source_filepath))
     print(call(r'scp -i {} {} {}@{}:{}'.format(source_key_file_path,target_key_file_path, source_username, source_ip, source_filepath).split(' ')))
     #enter the source server
-    print(call(r'ssh -i {} {}@{}'.format(source_key_file, source_username, source_ip)))
+    print(r'ssh -i {} {}@{}'.format(source_key_file_path, source_username, source_ip))
+    print(call(r'ssh -i {} {}@{}'.format(source_key_file_path, source_username, source_ip).split(' ')))
     #get target_key_file from the target_key_file_path
-    #local_target_key_file = target_key_file
+    target_key_file = target_key_file_path.split('/')[-1]
+    print('target_key_file: ', target_key_file)
     #set the target_key_file permissions to read and write
-    print(call(r'chmod 600 {}'.format(target_key_file)))
-    print(call(r'scp -i {} {} {}@{}:{}'.format(target_key_file, file_path, target_username, target_ip, target_directory)))
-    print(call(r'rm {}'.format(target_key_file)))
-    print(call('exit'))
+    print(call(r'chmod 600 {}'.format(target_key_file).split(' ')))
+    print(call(r'scp -i {} {} {}@{}:{}'.format(target_key_file, file_path, target_username, target_ip, target_directory).split(' ')))
+    print(call(r'rm {}'.format(target_key_file).split(' ')))
+    print(call('exit'.split()))
 #.format(ssh_key,file_path, target_ip)
-scp_from_into( source_key_file_path = r'C:\Users\krish\Downloads\inst-trial-2.pem', target_key_file_path = r'C:\Users\krish\Downloads\docker.pem', source_username = 'ubuntu', source_ip = '13.235.91.254', source_filepath = 'home/ubuntu/', file_path = 'home/ubuntu/laborum/laborum.py', target_username = 'root', target_ip = '134.209.148.94', target_directory = 'root/')
+scp_from_into( source_key_file_path = r'C:/Users/krish/Downloads/inst-trial-2.pem', target_key_file_path = r'C:/Users/krish/Downloads/docker.pem', source_username = 'ubuntu', source_ip = '13.235.91.254', source_filepath = 'home/ubuntu/', file_path = 'home/ubuntu/laborum/laborum.py', target_username = 'root', target_ip = '134.209.148.94', target_directory = 'root/')
