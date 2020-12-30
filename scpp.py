1b2aa869a449cfcd7c45f9694abaa2c7688e31fa
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2020-12-30 03:07:58 -0800

    Add files via upload

--- scpp.py
+++ scpp.py
@@ -3,18 +3,17 @@ from scp import SCPClient
 import os
 
 def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_filepath, target_username, target_host, target_directory_path):
-    
+    filename = copy_filepath.split('/')[-1]
     def scp_to(scp_client, target_directory_path):
-        scp_client.put(copy_filepath)
+        scp_client.put(files = filename, remote_path = format(target_directory_path.strip()))##scp.SCPException: scp: root/: Is a directory
 
     def scp_from(scp_client, source_filepath):
-        print('source file-path: ',source_filepath)
-        scp_client.get(source_filepath)
+        scp_client.get(source_filepath.strip())
 
     def ssh(ssh_file, hostname, username):
 
         client = paramiko.SSHClient()
-        key = paramiko.RSAKey.from_private_key_file(filename = source_ssh_file)
+        key = paramiko.RSAKey.from_private_key_file(filename = ssh_file)
         #client.load_system_host_keys(filename = ssh_file)
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname = hostname, username = username, port = 22, pkey = key)
@@ -30,12 +29,12 @@ def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_fil
     ssh_target = ssh(ssh_file = target_ssh_file, hostname = target_host, username = target_username)
     target_scp_client = SCPClient(ssh_target.get_transport())
     scp_to(target_scp_client, target_directory_path)
-    os.remove(copy_filepath.split('/')[-1])
+    os.remove(filename)
     
 
     #with SCPClient(ssh.get_transport()) as scp:
      #   scp.get(files = files)
-    ssh.close()
-    scp.close()
+    ssh_source.close()
+    ssh_target.close()
 
-scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '35.154.136.169', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '~/laborum/laborum.py', target_username = 'root', target_host = '134.209.148.94', target_directory_path = 'root/')
+scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '13.235.77.46', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/linkedin/README.md', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/')
