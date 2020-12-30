42ec692bd0ea10f2252cb2aad124701aa380e243
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2020-12-29 20:07:54 -0800

    Add files via upload

--- scpp.py
+++ scpp.py
@@ -4,9 +4,11 @@ from scp import SCPClient
 def scp_into(ssh_file, hostname, files = r'C:\Users\krish\Downloads\Affiliation_BYE_LAWS.doc'):
     def ssh(ssh_file, hostname):
         client = paramiko.SSHClient()
-        client.load_system_host_keys(filename = ssh_file)
+        key = paramiko.RSAKey.from_private_key_file(filename = ssh_file)
+        #client.load_system_host_keys(filename = ssh_file)
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname = hostname, port = 22, key_filename = ssh_file )#########check all parameters to be entered.
+        client.connect(hostname = hostname, username = 'ubuntu', port = 22, pkey = key)#########check all parameters to be entered.
+        print('connected.')
         return client
 
     ssh = ssh(ssh_file, hostname)
@@ -15,4 +17,4 @@ def scp_into(ssh_file, hostname, files = r'C:\Users\krish\Downloads\Affiliation_
         scp.put(files = files)
 
 
-scp_into(ssh_file = r'C:\Users\krish\Downloads\docker.pem', hostname = r'root@134.209.148.94')
+scp_into(ssh_file = r'C:\Users\krish\Downloads\inst-trial-2.pem', hostname = '65.0.19.49')
--- scpp2.py
+++ scpp2.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import *
 def scp_from_into(source_username, source_ip, source_filepath, file_path, target_username, target_ip, target_directory, source_key_file_path = None, target_key_file_path = None):#source_ip, target_ip, file_path, ssh_key
     #print(call(r'scp -i C:\Users\krish\Downloads\docker.pem  C:\Users\krish\Downloads\Affiliation_BYE_LAWS.doc root@134.209.148.94:/root/'.split(' ')))
     #copy target_key_file to the source
@@ -6,8 +6,9 @@ def scp_from_into(source_username, source_ip, source_filepath, file_path, target
         source_key_file_path = target_key_file_path
     elif source_key_file_path and not target_key_file_path:#If only source_key_file_path is given, same key is used for target_key_file_path
         target_key_file_path = source_key_file_path
-    print(r'scp -i {} {} {}@{}:{}'.format(source_key_file_path,target_key_file_path, source_username, source_ip, source_filepath))
-    print(call(r'scp -i {} {} {}@{}:{}'.format(source_key_file_path,target_key_file_path, source_username, source_ip, source_filepath).split(' ')))
+    #print(r'scp -i {} {} {}@{}:{}'.format(source_key_file_path,target_key_file_path, source_username, source_ip, source_filepath))
+    out = getoutput(r'scp -i {} {} {}@{}:{}'.format(source_key_file_path,target_key_file_path, source_username, source_ip, source_filepath).split(' '))
+    print('out: ',out)
     #enter the source server
     print(r'ssh -i {} {}@{}'.format(source_key_file_path, source_username, source_ip))
     print(call(r'ssh -i {} {}@{}'.format(source_key_file_path, source_username, source_ip).split(' ')))
@@ -20,4 +21,4 @@ def scp_from_into(source_username, source_ip, source_filepath, file_path, target
     print(call(r'rm {}'.format(target_key_file).split(' ')))
     print(call('exit'.split()))
 #.format(ssh_key,file_path, target_ip)
-scp_from_into( source_key_file_path = r'C:/Users/krish/Downloads/inst-trial-2.pem', target_key_file_path = r'C:/Users/krish/Downloads/docker.pem', source_username = 'ubuntu', source_ip = '13.235.91.254', source_filepath = 'home/ubuntu/', file_path = 'home/ubuntu/laborum/laborum.py', target_username = 'root', target_ip = '134.209.148.94', target_directory = 'root/')
+scp_from_into( source_key_file_path = r'C:/Users/krish/Downloads/inst-trial-2.pem', target_key_file_path = r'C:/Users/krish/Downloads/docker.pem', source_username = 'ubuntu', source_ip = '65.0.19.49', source_filepath = 'home/ubuntu/', file_path = 'home/ubuntu/laborum/laborum.py', target_username = 'root', target_ip = '134.209.148.94', target_directory = 'root/')
