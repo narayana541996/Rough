52ddc670e06b1acd4f271b0a37c58b43dbea60f6
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2020-12-31 00:08:04 -0800

    Add files via upload

--- scpp.py
+++ scpp.py
@@ -1,36 +1,39 @@
 import paramiko
 from scp import SCPClient
 import os
+import shutil
 
-def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_filepath, target_username, target_host, target_directory_path):
+def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_filepath, target_username, target_host, target_directory_path, recursive):
     filename = copy_filepath.split('/')[-1]
-    def scp_to(scp_client, target_directory_path):
-        scp_client.put(files = filename, remote_path = format(target_directory_path.strip()))##scp.SCPException: scp: root/: Is a directory
+    def scp_to(scp_client, target_directory_path, recursive):
+        print('working on it...')
+        scp_client.put(files = filename, remote_path = format(target_directory_path.strip()), recursive = recursive)##scp.SCPException: scp: root/: Is a directory
 
-    def scp_from(scp_client, source_filepath):
-        scp_client.get(source_filepath.strip())
+    def scp_from(scp_client, source_filepath, recursive):
+        print('working on it...')
+        scp_client.get(remote_path = source_filepath.strip(), recursive = recursive)
 
     def ssh(ssh_file, hostname, username):
 
         client = paramiko.SSHClient()
-        print('ssh_file: ',ssh_file)
+        #print('ssh_file: ',ssh_file)
         key = paramiko.RSAKey.from_private_key_file(filename = ssh_file)
-        #client.load_system_host_keys(filename = ssh_file)
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname = hostname, username = username, port = 22, pkey = key)
-        print('connected.')
-        #print(client.exec_command('scp -i {} {} {}@{}:{}'.format(target_key.split('/')[-1], copy_filepath, target_username, target_host, target_directory_path)))
-        #print(client.exec_command('rm {}'.format(target_key)))
-        #print('ls: ',client.exec_command('ls'))
+        print(hostname,' connected.')
         return client
 
     ssh_source = ssh(ssh_file = source_ssh_file, hostname = source_host, username = source_username)
     source_scp_client = SCPClient(ssh_source.get_transport())
-    scp_from(source_scp_client, copy_filepath)
+    scp_from(source_scp_client, copy_filepath, recursive)
     ssh_target = ssh(ssh_file = target_ssh_file, hostname = target_host, username = target_username)
     target_scp_client = SCPClient(ssh_target.get_transport())
-    scp_to(target_scp_client, target_directory_path)
-    os.remove(filename)
+    scp_to(target_scp_client, target_directory_path, recursive)
+    if os.path.isdir(filename):
+        shutil.rmtree(filename)
+    if os.path.isfile(filename):
+        os.remove(filename)
+    print('Done!')
     
 
     #with SCPClient(ssh.get_transport()) as scp:
@@ -38,4 +41,4 @@ def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_fil
     ssh_source.close()
     ssh_target.close()
 if __name__ == '__main__':
-    scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '52.66.31.152', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/laborum/laborum.py', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/')
+    scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '52.66.31.152', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/laborum/laborum.py', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/', recursive = True)
--- scpp_gui.py
+++ scpp_gui.py
@@ -1,10 +1,13 @@
 from tkinter import *
 import scpp
+import threading
 
-def local_scp(source_ssh_file_entry, source_username_entry, source_hostname_entry, target_ssh_file_entry, copy_filepath_entry, target_username_entry, target_hostname_entry, target_folderpath_entry):
-    scpp.scp(source_ssh_file = source_ssh_file_entry.get().strip().replace('\\','/'), source_username = source_username_entry.get().strip().replace('\\','/'), source_host = source_hostname_entry.get().strip().replace('\\','/'), target_ssh_file = target_ssh_file_entry.get().strip().replace('\\','/'), copy_filepath = copy_filepath_entry.get().strip().replace('\\','/'), target_username = target_username_entry.get().strip().replace('\\','/'), target_host = target_hostname_entry.get().strip().replace('\\','/'), target_directory_path = target_folderpath_entry.get().strip().replace('\\','/'))
+def local_scp(source_ssh_file_entry, source_username_entry, source_hostname_entry, target_ssh_file_entry, copy_filepath_entry, target_username_entry, target_hostname_entry, target_folderpath_entry, recursive):
+    scpp.scp(source_ssh_file = source_ssh_file_entry.get().strip().replace('\\','/'), source_username = source_username_entry.get().strip().replace('\\','/'), source_host = source_hostname_entry.get().strip().replace('\\','/'), target_ssh_file = target_ssh_file_entry.get().strip().replace('\\','/'), copy_filepath = copy_filepath_entry.get().strip().replace('\\','/'), target_username = target_username_entry.get().strip().replace('\\','/'), target_host = target_hostname_entry.get().strip().replace('\\','/'), target_directory_path = target_folderpath_entry.get().strip().replace('\\','/'), recursive = recursive.get())
+
 
 main = Tk()
+recursive = BooleanVar(main)
 #copy_type_radiobuttons = Radiobutton(main, )
 source_hostname_label = Label(main, text = 'Source IP/Hostname: ')
 source_hostname_label.grid(row = 1, column = 0, padx = 4, pady = 4, sticky = 'w')
@@ -36,16 +39,18 @@ target_ssh_file_label.grid(row = 5, column = 1, padx = 4, pady = 4, sticky = 'w'
 target_ssh_file_entry = Entry(main, width = 50)
 target_ssh_file_entry.grid(row = 6, column = 1, padx = 4, pady = 4, sticky = 'w')
 
-copy_filepath_label = Label(main, text = 'Absolute Path of File to be copied: ')
+copy_filepath_label = Label(main, text = 'Absolute Path of File to be Copied: ')
 copy_filepath_label.grid(row = 7, column =0, padx = 4, pady = 4, sticky = 'w')
 copy_filepath_entry = Entry(main, width = 50)
 copy_filepath_entry.grid(row = 8, column = 0 , padx = 4, pady = 4, sticky = 'w')
 
-target_folderpath_label = Label(main, text = 'Absolute Path to the Folder in which the file is to be saved:')
+target_folderpath_label = Label(main, text = 'Absolute Path of the Destination Folder:')
 target_folderpath_label.grid(row = 7, column = 1, padx = 4, pady = 4, sticky = 'w')
 target_folderpath_entry = Entry(main, width = 50)
 target_folderpath_entry.grid(row = 8, column = 1, padx = 4, pady = 4, sticky = 'w')
 ###add print statements to check the cause of the empty string
-copy_button = Button(main, text = 'Copy', command = lambda source_ssh_file_entry = source_ssh_file_entry, source_username_entry = source_username_entry, source_hostname_entry = source_hostname_entry, target_ssh_file_entry = target_ssh_file_entry, copy_filepath_entry = copy_filepath_entry, target_username_entry = target_username_entry, target_hostname_entry = target_hostname_entry, target_folderpath_entry = target_folderpath_entry: local_scp(source_ssh_file_entry, source_username_entry, source_hostname_entry, target_ssh_file_entry, copy_filepath_entry, target_username_entry, target_hostname_entry, target_folderpath_entry))
+copy_button = Button(main, text = 'Copy', command = lambda source_ssh_file_entry = source_ssh_file_entry, source_username_entry = source_username_entry, source_hostname_entry = source_hostname_entry, target_ssh_file_entry = target_ssh_file_entry, copy_filepath_entry = copy_filepath_entry, target_username_entry = target_username_entry, target_hostname_entry = target_hostname_entry, target_folderpath_entry = target_folderpath_entry, recursive = recursive: threading.Thread(target = local_scp, args = (source_ssh_file_entry, source_username_entry, source_hostname_entry, target_ssh_file_entry, copy_filepath_entry, target_username_entry, target_hostname_entry, target_folderpath_entry, recursive)).start())
 copy_button.grid(row = 9, column = 0, padx = 4, pady = 4, columnspan = 2, sticky = 'nsew')
+recursive_checkbutton = Checkbutton(main, text = 'Recursive', variable = recursive, onvalue = True, offvalue = False)
+recursive_checkbutton.grid(row = 10, column = 0, padx = 4, pady = 4, columnspan = 2, sticky = 'w')
 main.mainloop()
