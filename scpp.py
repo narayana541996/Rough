6695d84788a306c917f24b8c1477dcf9edecf45f
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2021-01-18 02:38:51 -0800

    Add files via upload
    -Added Trust and Copy.
    -Working on establishing trust between source and target.

--- scpp.py
+++ scpp.py
@@ -4,7 +4,7 @@ import os
 import shutil
 from subprocess import *
 
-def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_filepath, target_username, target_host, target_directory_path, recursive):
+def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_filepath, target_username, target_host, target_directory_path, recursive, establish_trust = True):
     filename = copy_filepath.split('/')[-1]
     def scp_to(scp_client, target_directory_path, recursive):
         print('working on it...')
@@ -30,21 +30,33 @@ def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_fil
     ssh_target = ssh(ssh_file = target_ssh_file, hostname = target_host, username = target_username)
     target_scp_client = SCPClient(ssh_target.get_transport())
     scp_to(target_scp_client, target_directory_path, recursive)
-    copy_key(source_ssh_file, target_username, target_host)#######replace source path with the path in the other server
+    if establish_trust:
+        copy_key(ssh_source, source_ssh_file, target_username, target_host)#######replace source path with the path in the other server
+        print('Established trust.')
     if os.path.isdir(filename):
         shutil.rmtree(filename)
     if os.path.isfile(filename):
         os.remove(filename)
     print('Done!')
 
-def copy_key(ssh_file, target_username, target_ip):
-    call('ssh-copy-id -i {} {}@{}'.format(ssh_file, target_username, target_ip))
+def copy_key(ssh_source, ssh_file, target_username, target_ip):#####might have to merge generate_key and copy_key.
+    #call('ssh-copy-id -i {} {}@{}'.format(ssh_file, target_username, target_ip))
+    #copy = Popen(['ssh-copy-id', '-i', ssh_file, f'{target_username}@{target_ip}'], shell = False, stdout = PIPE, stderr = PIPE)
+    stdin, stdout, stderr = ssh_source.exec_command('ssh-copy-id -i {} {}@{}'.format(ssh_file, target_username, target_ip))
+    print('copy stdout: ', stdout)
+    return stdout
 
 def generate_key():
-    call(['ssh-keygen'])
+    #call(['ssh-keygen'])
+    key_gen = Popen(['ssh-keygen'], shell = False, stdout = PIPE, stderr = PIPE)
+    key = key_gen.stdout.readlines()
+    if key == []:
+        key = key_gen.stderr.readlines()
+    print('generate stdout: ', stdout)
+    return key
     #with SCPClient(ssh.get_transport()) as scp:
      #   scp.get(files = files)
     ssh_source.close()
     ssh_target.close()
 if __name__ == '__main__':
-    scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '3.7.55.3', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/laborum/laborum.py', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/', recursive = True)
+    scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '13.127.7.213', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/laborum/laborum.py', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/', recursive = True)
