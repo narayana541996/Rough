c8a264785bb50f5a14484c59a9e0b3706fe66f38
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2021-01-28 03:49:25 -0800

    Add files via upload

--- scpp.py
+++ scpp.py
@@ -16,9 +16,11 @@ def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_fil
 
     def generate_key(client, create_key_filename, bits = '1024', create_key_password = None):
         #paramiko.RSAKey.generate(bits = int(bits)).write_private_key_file(create_key_filename, create_key_password)
-        if create_key_password == None:
-            create_key_password = ''
-        client.exec_command(f'ssh-keygen -b {bits} -f {create_key_filename} -N {create_key_password}')
+        if create_key_password == None or create_key_password == '':#######check if the file already exists.
+            stdin, stdout, stderr = client.exec_command(f'ssh-keygen -b {bits} -f ~/.ssh/{create_key_filename} -N {create_key_password}')
+        else:
+            stdin, stdout, stderr = client.exec_command(f'ssh-keygen -b {bits} -f ~/.ssh/{create_key_filename}')
+        print('generate_key:\nstdout: ', stdout.read(),' stderr: ', stderr.read())
         #return create_key_filename
 
     def ssh(ssh_file, hostname, username, password = None):
@@ -32,7 +34,8 @@ def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_fil
         except paramiko.AuthenticationException:
             try:
                 print('ssh_file: ',ssh_file)
-                os.chmod(ssh_file, 0o755)
+                #os.chmod('~/.ssh', 0o700)
+                os.chmod(ssh_file, 0o600)
                 #run(['chmod','0644', ssh_file])#cannot find file
                 print('run chmod')
                 client.connect(hostname = hostname, username = username, port = 22, pkey = key)
@@ -48,18 +51,19 @@ def scp(source_ssh_file, source_username, source_host, target_ssh_file, copy_fil
             print(hostname,' connected.')
         return client
 
-    ssh_source = ssh(ssh_file = source_ssh_file, hostname = source_host, username = source_username)
+    ssh_source = ssh(ssh_file = source_ssh_file, hostname = source_host, username = source_username, password = source_password)
     
     ssh_target = ssh(ssh_file = target_ssh_file, hostname = target_host, username = target_username, password = target_password)
     
     if establish_trust:
+        ####check if the trust is already established.
         generate_key(ssh_source, create_key_filename, bits, create_key_password)
         #key = open(generate_key(ssh_source, create_key_filename, bits, create_key_password)).read()
         #client.exec_command(f'ssh -i {target_ssh_file} {target_username}@{target_host}')
-        stdin1, stdout1, stderr1 = ssh_source.exec_command(f'ssh-copy-id -i {create_key_filename} {target_username}@{target_host}')
+        stdin1, stdout1, stderr1 = ssh_source.exec_command(f'ssh-copy-id -i ~/.ssh/{create_key_filename} {target_username}@{target_host}')
         #stdin1, stdout1, stderr1 = ssh_target.exec_command(f'echo "{key}" > ~/.ssh/authorized_keys')
         stdin2, stdout2, stderr2 = ssh_source.exec_command(f'ssh {target_username}@{target_host}')
-        print('stdin1: ',stdin1.read(),' stdout1: ',stdout1.read(),' stderr1: ',stderr1.read(),' stdin2: ', stdin2.read(),' stdout2: ', stdout2.read(),' stderr2: ', stderr2.read())
+        print('stdout1: ',stdout1.read(),' stderr1: ',stderr1.read(),' stdout2: ', stdout2.read(),' stderr2: ', stderr2.read())
         print('Established trust.')
     source_scp_client = SCPClient(ssh_source.get_transport())
     scp_from(source_scp_client, copy_filepath, recursive)
@@ -94,6 +98,6 @@ def copy_key(ssh_file, target_username, target_ip, target_password):
      #   scp.get(files = files)
     ssh_source.close()
     ssh_target.close()
-if __name__ == '__main__':
-    scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-2.pem', source_username ='ubuntu', source_host = '65.0.122.94', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/laborum/laborum.py', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/', recursive = True, target_password = '')
+if __name__ == '__main__':############################Link docker back to docker-machine.
+    scp(source_ssh_file = r'C:/Users/krish/Downloads/inst-trial-3.pem', source_username ='ubuntu', source_host = '13.232.65.183', source_password = '', target_ssh_file = r'C:/Users/krish/Downloads/docker.pem', copy_filepath = '/home/ubuntu/upload_test', target_username = 'root', target_host = '134.209.148.94', target_directory_path = '/root/', recursive = True, target_password = '')
     #copy_key(generate_key('key_file', '1024', None), target_username = 'root', target_ip = '134.209.148.94')
